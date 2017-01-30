12aebdaa3a6638a46b735d07bbec307aad40e440
bona <bjorn.fyrvall@gmail.com>
2017-01-30 22:44:54 +0100

    Fix for start page.
    Removed old sql and code.

--- Application/Sql/SiteOption.sql
+++
@@ -1,6 +0,0 @@
-create table SiteOption(
-  Id int not null primary key auto_increment,
-  Identifier varchar(128),
-  DisplayName varchar(512),
-  Value varchar(512)
-);
--- Application/Views/Pages/Page.php
+++ Application/Views/Pages/Page.php
@@ -1,9 +1,13 @@
-<h1><?php echo $Page->PageTitle;?></h1>
+<?php if($Page == null):?>
+    <h1>Start page not set</h1>
+<?php else:?>
+    <h1><?php echo $Page->PageTitle;?></h1>
 
-<?php foreach($Page->PageSegments->Where(array('IsDeleted' => 0, 'IsActive' => 1))->OrderBy('SortOrder') as $section):?>
-    <div class="row">
-        <div class="col-lg-12">
-            <?php echo $section->Content;?>
+    <?php foreach($Page->PageSegments->Where(array('IsDeleted' => 0, 'IsActive' => 1))->OrderBy('SortOrder') as $section):?>
+        <div class="row">
+            <div class="col-lg-12">
+                <?php echo $section->Content;?>
+            </div>
         </div>
-    </div>
-<?php endforeach;?>
+    <?php endforeach;?>
+<?php endif;?>
